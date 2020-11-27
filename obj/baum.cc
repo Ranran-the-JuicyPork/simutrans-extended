aburch/simutrans

FIX: map enlargement crashes with new climate generator
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9278 8aca7d54-2c30-db11-9de9-000461428c89

obj/baum.cc
@@ -46,7 +46,7 @@ static image_id tree_id_to_image[256][5*5];
 
 
 // distributes trees on a map
-void baum_t::distribute_trees(int dichte)
+void baum_t::distribute_trees(int dichte, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom )
 {
 	// now we can proceed to tree planting routine itself
 	// best forests results are produced if forest size is tied to map size -
@@ -62,10 +62,10 @@ DBG_MESSAGE("verteile_baeume()","creating %i forest",c_forest_count);
 		// to have same execution order for simrand
 		koord const start = koord::koord_random(x, y);
 		koord const size  = koord(t_forest_size,t_forest_size) + koord::koord_random(t_forest_size, t_forest_size);
-		create_forest( start, size );
+		create_forest( start, size, xtop, ytop, xbottom, ybottom );
 	}
 
-	fill_trees(dichte);
+	fill_trees( dichte, xtop, ytop, xbottom, ybottom );
 }
 
 
@@ -178,7 +178,7 @@ bool baum_t::plant_tree_on_coordinate(koord pos, const tree_desc_t *desc, const 
 }
 
 
-uint32 baum_t::create_forest(koord new_center, koord wh )
+uint32 baum_t::create_forest(koord new_center, koord wh, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom )
 {
 	// none there
 	if(  desc_table.empty()  ) {
@@ -192,6 +192,13 @@ uint32 baum_t::create_forest(koord new_center, koord wh )
 
 			const sint32 x_tree_pos = (j-(wh.x>>1));
 			const sint32 y_tree_pos = (i-(wh.y>>1));
+
+			if( xtop > x_tree_pos  ||  x_tree_pos >= xbottom ) {
+				continue;
+			}
+			if( ytop > y_tree_pos  ||  y_tree_pos >= ybottom ) {
+				continue;
+			}
 
 			const uint64 distance = 1 + sqrt_i64( ((uint64)x_tree_pos*x_tree_pos*(wh.y*wh.y) + (uint64)y_tree_pos*y_tree_pos*(wh.x*wh.x)));
 			const uint32 tree_probability = (uint32)( ( 8 * (uint32)((wh.x*wh.x)+(wh.y*wh.y)) ) / distance );
@@ -216,7 +223,7 @@ uint32 baum_t::create_forest(koord new_center, koord wh )
 }
 
 
-void baum_t::fill_trees(int dichte)
+void baum_t::fill_trees(int dichte, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom )
 {
 	// none there
 	if(  desc_table.empty()  ) {
@@ -224,8 +231,8 @@ void baum_t::fill_trees(int dichte)
 	}
 DBG_MESSAGE("verteile_baeume()","distributing single trees");
 	koord pos;
-	for(  pos.y=0;  pos.y<welt->get_size().y;  pos.y++  ) {
-		for(  pos.x=0;  pos.x<welt->get_size().x;  pos.x++  ) {
+	for(  pos.y=ytop;  pos.y<ybottom;  pos.y++  ) {
+		for(  pos.x=xtop;  pos.x<xbottom;  pos.x++  ) {
 			grund_t *gr = welt->lookup_kartenboden(pos);
 			if(gr->get_top() == 0  &&  gr->get_typ() == grund_t::boden)  {
 				// plant spare trees, (those with low preffered density) or in an entirely tree climate

obj/baum.h
@@ -113,7 +113,11 @@ public:
 	// static functions to handle trees
 
 	// distributes trees on a map
-	static void distribute_trees(int dichte);
+	static void distribute_trees(int dichte,  sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom );
+
+	static void fill_trees(int dichte, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom );
+
+	static uint32 create_forest(koord center, koord size, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom );
 
 	static bool plant_tree_on_coordinate(koord pos, const tree_desc_t *desc, const bool check_climate, const bool random_age );
 
@@ -121,9 +125,6 @@ public:
 
 	static bool register_desc(tree_desc_t *desc);
 	static bool successfully_loaded();
-
-	static uint32 create_forest(koord center, koord size );
-	static void fill_trees(int dichte);
 
 	// return list to descriptors
 	static vector_tpl<tree_desc_t const*> const& get_all_desc() { return tree_list; }

Ranran-the-JuicyPork/simutrans-extended

git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9278
8aca7d54-2c30-db11-9de9-000461428c89
# Conflicts:
#	obj/baum.cc
#	obj/baum.h
#	simutrans/history.txt
#	simworld.cc

obj/baum.cc
@@ -47,7 +47,7 @@ static image_id tree_id_to_image[256][5*5];
 
 
 // distributes trees on a map
-void baum_t::distribute_trees(int density)
+void baum_t::distribute_trees(int density, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom )
 {
 	// now we can proceed to tree planting routine itself
 	// best forests results are produced if forest size is tied to map size -
@@ -64,11 +64,11 @@ DBG_MESSAGE("verteile_baeume()","creating %i forest",c_forest_count);
 		// to have same execution order for simrand
 		koord const start = koord::koord_random(x, y);
 		koord const size  = koord(t_forest_size,t_forest_size) + koord::koord_random(t_forest_size, t_forest_size);
-		create_forest( start, size );
+		create_forest( start, size, xtop, ytop, xbottom, ybottom );
 		ls.set_progress( c1+1 );
 	}
 
-	fill_trees(density);
+	fill_trees( density, xtop, ytop, xbottom, ybottom );
 }
 
 
@@ -183,7 +183,7 @@ bool baum_t::plant_tree_on_coordinate(koord pos, const tree_desc_t *desc, const 
 }
 
 
-uint32 baum_t::create_forest(koord new_center, koord size )
+uint32 baum_t::create_forest(koord new_center, koord size, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom )
 {
 	// none there
 	if(  desc_names.empty()  ) {
@@ -197,6 +197,13 @@ uint32 baum_t::create_forest(koord new_center, koord size )
 
 			const sint32 x_tree_pos = (j-(size.x>>1));
 			const sint32 y_tree_pos = (i-(size.y>>1));
+
+			if( xtop > x_tree_pos  ||  x_tree_pos >= xbottom ) {
+				continue;
+			}
+			if( ytop > y_tree_pos  ||  y_tree_pos >= ybottom ) {
+				continue;
+			}
 
 			const uint64 distance = 1 + sqrt_i64( ((uint64)x_tree_pos*x_tree_pos*(size.y*size.y) + (uint64)y_tree_pos*y_tree_pos*(size.x*size.x)));
 			const uint32 tree_probability = (uint32)( ( 8 * (uint32)((size.x*size.x)+(size.y*size.y)) ) / distance );
@@ -221,7 +228,7 @@ uint32 baum_t::create_forest(koord new_center, koord size )
 }
 
 
-void baum_t::fill_trees(int density)
+void baum_t::fill_trees(int density, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom )
 {
 	// none there
 	if(  desc_names.empty()  ) {
@@ -229,8 +236,8 @@ void baum_t::fill_trees(int density)
 	}
 DBG_MESSAGE("verteile_baeume()","distributing single trees");
 	koord pos;
-	for(  pos.y=0;  pos.y<welt->get_size().y;  pos.y++  ) {
-		for(  pos.x=0;  pos.x<welt->get_size().x;  pos.x++  ) {
+	for(  pos.y=ytop;  pos.y<ybottom;  pos.y++  ) {
+		for(  pos.x=xtop;  pos.x<xbottom;  pos.x++  ) {
 			grund_t *gr = welt->lookup_kartenboden(pos);
 			if(gr->get_top() == 0  &&  gr->get_typ() == grund_t::boden)  {
 				// plant spare trees, (those with low preffered density) or in an entirely tree climate

obj/baum.h
@@ -124,7 +124,11 @@ public:
 	// static functions to handle trees
 
 	// distributes trees on a map
-	static void distribute_trees(int density);
+	static void distribute_trees(int density,  sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom );
+
+	static void fill_trees(int density, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom );
+
+	static uint32 create_forest(koord center, koord size, sint16 xtop, sint16 ytop, sint16 xbottom, sint16 ybottom );
 
 	static bool plant_tree_on_coordinate(koord pos, const tree_desc_t *desc, const bool check_climate, const bool random_age );
 
@@ -133,10 +137,7 @@ public:
 	static bool register_desc(tree_desc_t *desc);
 	static bool successfully_loaded();
 
-	static uint32 create_forest(koord center, koord size );
-	static void fill_trees(int density);
-
-	// return list to descs
+	// return list to descriptors
 	static vector_tpl<tree_desc_t const*> const& get_all_desc() { return tree_list; }
 
 	static const tree_desc_t *random_tree_for_climate(climate cl) { uint8 b = random_tree_for_climate_intern(cl);  return b!=invalid_tree_id ? tree_list[b] : NULL; }